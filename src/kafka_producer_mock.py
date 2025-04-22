b9b3d95ac565c88cbaf8192f23d5162090bd1041
Cauai Capozzoli <cauaiagro@gmail.com>
2025-04-22 14:05:51 -0300

    fixing issues

--- src/kafka_producer_mock.py
+++ src/kafka_producer_mock.py
@@ -3,18 +3,21 @@ import json
 import random
 import time
 
-
-
 def create_producer():
     return KafkaProducer(
-        bootsrap_servers=['localhost:9092'],
-        value_serializer=lambda v: json.dumpls(v).encode('utf-8'))
+        bootstrap_servers=['localhost:9092'],  # Corrigido
+        value_serializer=lambda v: json.dumps(v).encode('utf-8')  # Corrigido
+    )
 
 def generate_mock_message(order_id):
     return {
         "order_number": order_id,
         "order_items": [
-            {"item_id": i, "qty": random.randint(1, 5), "value_unit": round(random.uniform(10, 500), 2)}
+            {
+                "item_id": i,
+                "qty": random.randint(1, 5),
+                "value_unit": round(random.uniform(10, 500), 2)
+            }
             for i in range(random.randint(1, 5))
         ]
     }
@@ -32,4 +35,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-                          
